b9aa61800b3393db614c8185ecb96eaceeec7022
Donna Paige <heypaigedesign@gmail.com>
2026-06-02 10:45:38 +0800

    Add HubSpot and LinkedIn Insight Tag tracking scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Archivo, Space_Mono } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import Nav from '@/components/Nav'
 import Footer from '@/components/Footer'
@@ -38,6 +39,43 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Nav />
         {children}
         <Footer />
+
+        {/* HubSpot */}
+        <Script
+          id="hs-script-loader"
+          src="//js-na2.hs-scripts.com/246365096.js"
+          strategy="afterInteractive"
+        />
+
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-insight" strategy="afterInteractive">{`
+          _linkedin_partner_id = "9466316";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          (function(l) {
+            if (!l) {
+              window.lintrk = function(a, b) { window.lintrk.q.push([a, b]) };
+              window.lintrk.q = [];
+            }
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";
+            b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);
+          })(window.lintrk);
+        `}</Script>
+        {/* LinkedIn noscript fallback */}
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=9466316&fmt=gif"
+          />
+        </noscript>
       </body>
     </html>
   )
